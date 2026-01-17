ad0833aadd3f50687979c2006b67955f191d9ca5
citauka <daryaatsitova@icloud.com>
2026-01-18 00:05:28 +0100

    Fix logger for production: disable pino-pretty in production mode

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,16 +2,21 @@
 
 import { pinoHttp } from 'pino-http';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const logger = pinoHttp({
   level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
+  // Используем pino-pretty только в development
+  ...(isProduction ? {} : {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
     },
-  },
+  }),
 });
