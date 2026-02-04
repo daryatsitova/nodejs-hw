76a918b8a650d6bf27a658b0ce4df0fbe7b43dfa
citauka <daryaatsitova@icloud.com>
2026-02-04 20:27:11 +0100

    search

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -21,7 +21,7 @@ export const getAllNotes = async (req, res) => {
   }
 
   if (search) {
-    notesQuery.find({ $text: { $search: search } });
+    notesQuery.where({ $text: { $search: search } });
   }
 
   const [totalNotes, notes] = await Promise.all([
