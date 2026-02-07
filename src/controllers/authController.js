9968a7c3d8bbd5eb990113f215dfba5e2c40c000
citauka <daryaatsitova@icloud.com>
2026-02-07 01:48:03 +0100

    reset email base

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,11 @@ import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import bcrypt from 'bcrypt';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendMail.js';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -95,4 +100,50 @@ export const logoutUser = async (req, res) => {
   res.clearCookie('refreshToken');
 
   res.status(204).send();
+};
+
+
+export const requestResetEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(200).json({
+      message: 'If this email exists, a reset link has been sent',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+	// 1. Формуємо шлях до шаблона
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+  // 2. Читаємо шаблон
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+  // 3. Готуємо шаблон до заповнення
+  const template = handlebars.compile(templateSource);
+  // 4. Формуємо із шаблона HTML документ з динамічними даними
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      // 5. Передаємо HTML у функцію надписання пошти
+      html,
+    });
+  } catch {
+    throw createHttpError(500, 'Failed to send the email, please try again later.');
+  }
+
+  res.status(200).json({
+    message: 'If this email exists, a reset link has been sent',
+  });
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser, refreshUserSession, logoutUser} from '../controllers/authController.js';
-import { registerUserSchema, loginUserSchema} from '../validations/authValidation.js';
+import { registerUser, loginUser, refreshUserSession, logoutUser, requestResetEmail} from '../controllers/authController.js';
+import { registerUserSchema, loginUserSchema, requestResetEmailSchema} from '../validations/authValidation.js';
 
 
 const router = Router();
@@ -11,5 +11,6 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/refresh', refreshUserSession);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/request-reset-email', celebrate(requestResetEmailSchema), requestResetEmail);
 
 export default router;
